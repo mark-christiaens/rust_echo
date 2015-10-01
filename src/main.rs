8a24ebbd1d5902d3b3820b50f329e6313eb9f9ca
Mark <mark.g.j.christiaens@gmail.com>
2015-10-01 21:01:18 +0000

    More gracious end of connection

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::net::{SocketAddr};
 use std::str::FromStr;
 
 const SERVER: Token = Token(0);
+type MyEventLoop = EventLoop<EchoServer>;
 
 struct EchoConn {
     sock: TcpStream,
@@ -30,54 +31,63 @@ impl EchoConn {
         }
     }
 
-    fn writable(&mut self, event_loop: &mut EventLoop<Echo>) -> io::Result<()> {
+    fn writable(&mut self, event_loop: &mut MyEventLoop) -> io::Result<()> {
         let mut buf = self.buf.take().unwrap();
 
         match self.sock.try_write_buf(&mut buf) {
             Ok(None) => {
-                print!("client flushing buf; WOULDBLOCK");
-
                 self.buf = Some(buf);
                 self.interest.insert(EventSet::writable());
             }
             Ok(Some(r)) => {
-                print!("CONN : we wrote {} bytes!", r);
+                let remaining = buf.remaining ();
 
-                self.mut_buf = Some(buf.flip());
-
-                self.interest.insert(EventSet::readable());
-                self.interest.remove(EventSet::writable());
+                if remaining > 0 {
+                    println!("we wrote {} bytes but {} remaining!", r, remaining);
+                    self.interest.insert(EventSet::writable());
+                }
+                else {
+                    println!("we wrote {} bytes, done", r);
+                    self.mut_buf = Some(buf.flip());
+                    self.interest.insert(EventSet::readable());
+                    self.interest.remove(EventSet::writable());
+                }
             }
-            Err(e) => print!("not implemented; client err={:?}", e),
+            Err(e) => println!("not implemented; client err={:?}", e),
         }
 
         event_loop.reregister(&self.sock, self.token.unwrap(), self.interest,
                               PollOpt::edge() | PollOpt::oneshot())
     }
 
-    fn readable(&mut self, event_loop: &mut EventLoop<Echo>) -> io::Result<()> {
+    fn readable(&mut self, event_loop: &mut MyEventLoop) -> io::Result<()> {
         let mut buf = self.mut_buf.take().unwrap();
 
         match self.sock.try_read_buf(&mut buf) {
             Ok(None) => {
-                panic!("We just got readable, but were unable to read from the socket?");
+                panic!("we just got readable, but were unable to read from the socket?");
             }
             Ok(Some(r)) => {
-                print!("CONN : we read {} bytes!", r);
-                self.interest.remove(EventSet::readable());
-                self.interest.insert(EventSet::writable());
+                println!("we read {} bytes!", r);
+                if r == 0 {
+                    println!("{:?}: end of connection", self.token.unwrap ());
+                    self.interest.remove(EventSet::readable());
+                }
+                else {
+                    self.interest.remove(EventSet::readable());
+                    self.interest.insert(EventSet::writable());
+ 
+                }
             }
             Err(e) => {
-                print!("not implemented; client err={:?}", e);
+                println!("not implemented; client err={:?}", e);
                 self.interest.remove(EventSet::readable());
             }
-
         };
-
+        
         // prepare to provide this to writable
         self.buf = Some(buf.flip());
-        event_loop.reregister(&self.sock, self.token.unwrap(), self.interest,
-                              PollOpt::edge())
+        event_loop.reregister(&self.sock, self.token.unwrap(), self.interest, PollOpt::edge())
     }
 }
 
@@ -87,32 +97,36 @@ struct EchoServer {
 }
 
 impl EchoServer {
-    fn accept(&mut self, event_loop: &mut EventLoop<Echo>) -> io::Result<()> {
-        print!("server accepting socket");
+    fn new(sock: TcpListener) -> EchoServer {
+        EchoServer {
+            sock: sock,
+            conns: Slab::new_starting_at(Token(2), 128)
+        }
+    }
+
+    fn accept(&mut self, event_loop: &mut MyEventLoop) -> io::Result<()> {
+        println!("server accepting socket");
 
         let sock = self.sock.accept().unwrap().unwrap();
-        let conn = EchoConn::new(sock,);
-        let tok = self.conns.insert(conn)
-            .ok().expect("could not add connection to slab");
+        let conn = EchoConn::new(sock);
+        let tok = self.conns.insert(conn).ok().expect("could not add connection to slab");
+        println! ("assigned token {:?}", tok);
 
         // Register the connection
         self.conns[tok].token = Some(tok);
-        event_loop.register_opt(&self.conns[tok].sock, tok, EventSet::readable(),
-                                PollOpt::edge() | PollOpt::oneshot())
+        event_loop.register_opt(&self.conns[tok].sock, tok, EventSet::readable(), PollOpt::edge() | PollOpt::oneshot())
             .ok().expect("could not register socket with event loop");
 
         Ok(())
     }
 
-    fn conn_readable(&mut self, event_loop: &mut EventLoop<Echo>,
-                     tok: Token) -> io::Result<()> {
-        print!("server conn readable; tok={:?}", tok);
+    fn conn_readable(&mut self, event_loop: &mut MyEventLoop, tok: Token) -> io::Result<()> {
+        println!("server conn readable; tok={:?}", tok);
         self.conn(tok).readable(event_loop)
     }
 
-    fn conn_writable(&mut self, event_loop: &mut EventLoop<Echo>,
-                     tok: Token) -> io::Result<()> {
-        print!("server conn writable; tok={:?}", tok);
+    fn conn_writable(&mut self, event_loop: &mut MyEventLoop, tok: Token) -> io::Result<()> {
+        println!("server conn writable; tok={:?}", tok);
         self.conn(tok).writable(event_loop)
     }
 
@@ -121,39 +135,23 @@ impl EchoServer {
     }
 }
 
-struct Echo {
-    server: EchoServer,
-}
-
-impl Echo {
-    fn new(sock: TcpListener) -> Echo {
-        Echo {
-            server: EchoServer {
-                sock: sock,
-                conns: Slab::new_starting_at(Token(2), 128)
-            },
-        }
-    }
-}
-
-impl Handler for Echo {
+impl Handler for EchoServer {
     type Timeout = usize;
     type Message = ();
 
-    fn ready(&mut self, event_loop: &mut EventLoop<Echo>, token: Token,
-             events: EventSet) {
-        print!("ready {:?} {:?}", token, events);
+    fn ready(&mut self, event_loop: &mut MyEventLoop, token: Token, events: EventSet) {
+        println!("ready {:?} {:?}", token, events);
         if events.is_readable() {
             match token {
-                SERVER => self.server.accept(event_loop).unwrap(),
-                i => self.server.conn_readable(event_loop, i).unwrap()
+                SERVER => self.accept(event_loop).unwrap(),
+                i => self.conn_readable(event_loop, i).unwrap()
             }
         }
 
         if events.is_writable() {
             match token {
-                SERVER => panic!("received writable for token 0"),
-                _ => self.server.conn_writable(event_loop, token).unwrap()
+                SERVER => panic!("received writable for SERVER: impossible, can only accept connections"),
+                _ => self.conn_writable(event_loop, token).unwrap()
             };
         }
     }
@@ -169,10 +167,13 @@ pub fn main () {
     let addr = localhost ();
     let srv_sock = TcpListener::bind(&addr).unwrap();
 
-    print!("listen for connections");
-    event_loop.register_opt(&srv_sock, SERVER, EventSet::readable(),
-                            PollOpt::edge() | PollOpt::oneshot()).unwrap();
+    println!("listen for connections");
+    event_loop.register_opt(
+        &srv_sock, SERVER, 
+        EventSet::readable(),
+        PollOpt::edge() | PollOpt::oneshot()
+    ).unwrap();
 
     // Start the event loop
-    event_loop.run(&mut Echo::new(srv_sock)).unwrap();
+    event_loop.run(&mut EchoServer::new(srv_sock)).unwrap();
 }
